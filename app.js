abd66376ae5afec0ae36929afced135cf137581d
muhammadsolih <muhammadsolih08091011@gmail.com>
2025-07-26 16:48:31 +0500

    main

---
+++ app.js
@@ -0,0 +1,92 @@
+// let a = 1;
+// let b = 15;
+// let natija = 0
+
+// for(let i = 1; i <= b; i++){
+//    natija++
+//    console.log(natija)
+// }
+
+// ! for 2
+
+// let a = 4;
+// let b = 15;
+// let natija = b;
+
+// if (a > b) {
+//    console.log("a < b")
+// }
+// else{
+//     for (let i = b -1; i > a; i--) {
+//     natija++;
+//     console.log(i)
+//    }
+//    console.log("oxirgi natija bu yani nacha martada chiqqani",natija);
+// }
+
+// ! for 3
+
+// let narx = 10000;
+// let i;
+// let pul = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   pul += narx;
+//   console.log(`${i} kg shokolad ${pul} ming`);
+// }
+
+// ! for 4
+
+// let narx = 10000;
+
+// for (let kg = 0.2; kg <= 2.0 ;kg += 0.2){
+//    let umumiyNarx = (narx * kg).toFixed(2);
+//    console.log(`${kg.toFixed(1)} kilogram konfir narxi ${umumiyNarx} sum`)
+// }
+
+
+
+// ! for 5;
+
+
+// let a = 1;
+// let b = 5;
+// let sum = 0;
+
+// for (let i = a; i < b; i++) {
+//   sum += i;
+//   console.log(sum);
+// }
+
+
+// ! for 6;
+
+
+// let a = 1;
+// let b = 10;
+// let c = a;
+
+// for (let i = a; i <= b; i++){
+//    c *= i
+// }
+// console.log(`a dan b gacha bolgan sonlar kopaytmasi: ${c}`)
+
+
+
+
+// ! for 7;
+
+
+let a = 1;
+let b = 5;
+let c = a
+
+for (let i = a; a <= b; i++){
+   c *= c;
+   console.log(c)
+}
+
+
+
+
+
